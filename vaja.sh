beb0441eb303dfed2cc7f9e1294702619b70c5ec
Ukiyyy <uros.mravljak@student.um.si>
2023-03-18 11:47:22 +0100

    added menu

--- vaja.sh
+++ vaja.sh
@@ -1,1 +1,12 @@
 #!/bin/bash
+file=imenik_26285.dat
+i=0
+if ! [ -f "$file" ]; then #file not exists
+    touch imenik_26285.dat
+fi 
+if [ -z $1 ];then
+	echo "-dodaj | dodajanje novega kontakta"
+	echo "-brisi | izbrisi kontakt"
+	echo "-isci | poisci kontakt"
+	echo "-uredi | uredi kontakt"
+fi
